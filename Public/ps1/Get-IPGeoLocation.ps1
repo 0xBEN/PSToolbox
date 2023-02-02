7a0dc3bf8f2d4ac248eae4028fc9540bc27e3356
Benjamin Heater <benheater@gmail.com>
2023-02-02 12:08:21 -0500

    Update Get-IPGeoLocation.ps1

--- Public/ps1/Get-IPGeoLocation.ps1
+++ Public/ps1/Get-IPGeoLocation.ps1
@@ -10,33 +10,23 @@ function Get-IPGeoLocation {
         [ValidateScript({
             
             if (-not ($_ -as [System.Net.IPAddress])) {
-
-                throw "Please pass a valid IPv4 address."
-
+                throw "Please pass a valid IP address."
             }
-            elseif (([System.Net.IPAddress]$_).AddressFamily -eq 'InterNetworkV6') {
-
-                throw "Please pass an IPv4 address."
-
-            }
-            else {
-
-                $splitIpAddress = $_.IPAddressToString.Split('.')
-                if (($splitIpAddress[0] -eq '10') -or (($splitIpAddress[0,1] -join '.') -eq '192.168') -or (($splitIpAddress[0] -eq '172') -and ($splitIpAddress[1] -in (16..31)))) { throw "Please pass a public IPv4 address" }
-                else { return $true }
-
-            }
-                   
+                  
         })]
         [System.Net.IPAddress]
-        $IPV4Address
+        $IPAddress
 
     )
     process {
-
-        $apiCall = Invoke-RestMethod -Headers @{'User-Agent' = @{'keycdn-tools' = 'https://null.com'}} -Uri "https://tools.keycdn.com/geo.json?host=$($IPV4Address.IPAddressToString)" -Method Get
-        return $apiCall.data.geo
-
+        
+        try {
+            Invoke-RestMethod "https://rdap.arin.net/registry/ip/$IPAddress" -ErrorAction Stop
+        }
+        catch {
+            throw $_.Exception
+        }
+        
     }
 
 }
