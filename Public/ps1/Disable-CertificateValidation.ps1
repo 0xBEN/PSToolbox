dc08009ba2d80598c7b60593fe24204dce2d6507
Benjamin Heater <benheater@gmail.com>
2023-05-16 13:00:43 -0400

    Update Disable-CertificateValidation.ps1

--- Public/ps1/Disable-CertificateValidation.ps1
+++ Public/ps1/Disable-CertificateValidation.ps1
@@ -1,30 +1,14 @@
 function Disable-CertificateValidation {
 
     [CmdletBinding()]
-    Param(
-
-    )
-    begin {
-
-        $class =
-@"
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-public static class DoNotValidateCertificates
-{
-    private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
-        SslPolicyErrors sslPolicyErrors) { return true; }
-    public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
-    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; }
-}
-"@
-
-    }
+    Param()
     process {
-
-        if (-not ([System.Management.Automation.PSTypeName]"DoNotValidateCertificates").Type) { Add-Type -TypeDefinition $class }
-        [DoNotValidateCertificates]::SetCallback()    
-
+        
+        if (-not ([System.Net.ServicePointManager]::ServerCertificateValidationCallback)) {
+        
+            [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+            
+        }
     }
 
 }
