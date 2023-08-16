7d2c4e7b6ac55e8682f5cb4bbd13ba8dfaa7a5b2
Benjamin Heater <benheater@gmail.com>
2023-08-16 15:10:15 -0400

    Update PSToolbox.psm1
    Complete rework of the .psm1 module script

--- PSToolbox.psm1
+++ PSToolbox.psm1
@@ -1,47 +1,49 @@
+$moduleName = $PSScriptRoot.Split([System.IO.Path]::DirectorySeparatorChar)[-1]
+$moduleManifest = "$PSScriptRoot\$moduleName.psd1"
+$currentManifest = Test-ModuleManifest $moduleManifest
+
+$aliases = @()
 $publicFunctions = Get-ChildItem -Path "$PSScriptRoot\Public\ps1" | Where-Object {$_.Extension -eq '.ps1'}
 $privateFunctions = Get-ChildItem -Path "$PSScriptRoot\Private\ps1" | Where-Object {$_.Extension -eq '.ps1'}
 $publicFunctions | ForEach-Object { . $_.FullName }
 $privateFunctions | ForEach-Object { . $_.FullName }
 
-$aliases = @()
-$publicFunctions | ForEach-Object { # Export all of the public functions from this module
-    
-    # The command has already been sourced in above. Query any defined aliases.
-    $alias = Get-Alias -Definition $_.BaseName -ErrorAction SilentlyContinue 
-    if ($alias) { # If alias string found (see example above)
-        $aliases += $alias
-        Export-ModuleMember -Function $_.BaseName -Alias $alias
-    }
-    else {
-        Export-ModuleMember -Function $_.BaseName        
-    }
-
-}
-
-$moduleName = $PSScriptRoot.Split([System.IO.Path]::DirectorySeparatorChar)[-1]
-$moduleManifest = "$PSScriptRoot\$moduleName.psd1"
-if ($PSVersionTable.PSEdition -ne 'Desktop') {
-    $currentManifest = pwsh -NoProfile -Command "Test-ModuleManifest '$moduleManifest'" # Unfortunate hack to test the module manifest for changes without having to reload PowerShell
-}
-else {
-    $currentManifest = powershell -NoProfile -Command "Test-ModuleManifest '$moduleManifest'" # Unfortunate hack to test the module manifest for changes without having to reload PowerShell
-}
 $functionsAdded = $publicFunctions | Where-Object {$_.BaseName -notin $currentManifest.ExportedFunctions.Keys}
 $functionsRemoved = $currentManifest.ExportedFunctions.Keys | Where-Object {$_ -notin $publicFunctions.BaseName}
 $aliasesAdded = $aliases | Where-Object {$_ -notin $currentManifest.ExportedAliases.Keys}
 $aliasesRemoved = $currentManifest.ExportedAliases.Keys | Where-Object {$_ -notin $aliases}
-if ($functionsAdded -or $functionsRemoved -or $aliasesAdded -or $aliasesRemoved) { 
+
+$publicFunctions | ForEach-Object { # Export all of the public functions from this module
+
+    # The command has already been sourced in above. Query any defined aliases.
+    $alias = Get-Alias -Definition $_.BaseName -ErrorAction SilentlyContinue
+    if ($alias) {
+        $aliases += $alias
+        Export-ModuleMember -Function $_.BaseName -Alias $alias
+    }
+    else {
+        Export-ModuleMember -Function $_.BaseName
+    }
+
+}
+
+if ($functionsAdded -or $functionsRemoved -or $aliasesAdded -or $aliasesRemoved) {
+
     try {
 
-        if ($aliasesAdded) {
-            Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $publicFunctions.BaseName -AliasesToExport $aliases -ErrorAction Stop
-        }
-        else {
-            Update-ModuleManifest -Path $moduleManifest -FunctionsToExport $publicFunctions.BaseName -AliasesToExport @() -ErrorAction Stop
-        }
+        $updateModuleManifestParams = @{}
+        $updateModuleManifestParams.Add('Path', $moduleManifest)
+        $updateModuleManifestParams.Add('ErrorAction', 'Stop')
+        if ($aliases.Count -gt 0) { $updateModuleManifestParams.Add('AliasesToExport', $aliases) }
+        if ($publicFunctions.Count -gt 0) { $updateModuleManifestParams.Add('FunctionsToExport', $publicFunctions.BaseName) }
+
+        Update-ModuleManifest @updateModuleManifestParams
 
     }
     catch {
-        # Empty to silence errors
+
+        $_ | Write-Error
+
     }
+
 }
