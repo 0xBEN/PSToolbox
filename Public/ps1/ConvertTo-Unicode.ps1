ef5ab42cf95816551f4d12f75c4200ceff65670d
Benjamin Heater <benheater@gmail.com>
2024-03-28 14:54:29 -0400

    Create ConvertTo-Unicode.ps1

---
+++ Public/ps1/ConvertTo-Unicode.ps1
@@ -0,0 +1,25 @@
+function ConvertTo-Unicode {
+
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [ValidateNotNullOrEmpty()]
+    [String[]]$InputString
+  )
+
+  begin {}
+  process {
+    
+    $InputString | ForEach-Object {
+      $string = $_
+      $charArray = $string.ToCharArray()
+      foreach ($char in $charArray) {
+        return '/u{0:x4}' -f [byte][char]$char
+      }
+      
+    }
+    
+  }
+  end {}
+
+}
